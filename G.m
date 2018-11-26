d7306294d31aea9cb6c4e5cde077494748286e8a
r0n4n <ronan.blanchard35@gmail.com>
2018-11-26 20:59:15 +0100

    ajout de la function G(x)

---
+++ G.m
@@ -0,0 +1,35 @@
+function y = G(x,m,I)
+
+Ix = I(1);
+Iy = I(2);
+Iz = I(3);
+
+G1 = zeros(12,1) ; 
+G2 = zeros(12,1) ; 
+G3 = zeros(12,1) ; 
+G4 = zeros(12,1) ;
+
+G1(7)=  -(1/m)*(sin(x(6))*sin(x(4))+cos(x(6))*cos(x(4))*sin(x(5))) ; 
+G1(8)=  -(1/m)*(cos(x(4))*sin(x(6))-cos(x(6))*sin(x(4))*sin(x(5))); 
+G1(9) =  - (1/m)*cos(x(6))*cos(x(5)); 
+
+G2(10) = 1/Ix ; 
+G3(11) = 1/Iy ; 
+G4(12) = 1/Iz ; 
+
+y = [G1 G2 G3 G4] ;
+
+% y = [0;
+%     0;
+%     0;
+%     0;
+%     0;
+%     0;
+%     -(1/m)*(sin(x(6))*sin(x(4))+cos(x(6))*cos(x(4))*sin(x(5)));
+%     -(1/m)*(cos(x(4))*sin(x(6))-cos(x(6))*sin(x(4))*sin(x(5)));
+%     - (1/m)*cos(x(6))*cos(x(5));
+%     (1/Ix);
+%     (1/Iy);
+%     (1/Iz)];
+end
+
