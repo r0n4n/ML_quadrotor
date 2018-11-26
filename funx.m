ad1a5681bfb22ac21db28431a2dfd9a2f8474baf
r0n4n <ronan.blanchard35@gmail.com>
2018-11-26 21:00:50 +0100

    correction sur le calcul de p point et modification de G

--- funx.m
+++ funx.m
@@ -1,29 +1,44 @@
-function dydt = funx(t,y)
+function dydt = funx(t,y,g,m,I,R,Q)
 % Définition des paramètres Ix, Iy, Iz :
-g = 9.81;
-m = 0.25;
-Ix = 1.2;
-Iy = 1.1;
-Iz = 1;
+%g = 9.81;
+%disp(g)
+%m = 0.25;
+Ix = I(1);
+Iy = I(2);
+Iz = I(3);
 
-%matrice de pondération
-R=eye(1);
-Q= eye(1);
+% %matrice de pondération
+% R=eye(1);
+% Q= eye(1);
 
 % calcul de x point
+G1 = zeros(12,1) ; 
+G2 = zeros(12,1) ; 
+G3 = zeros(12,1) ; 
+G4 = zeros(12,1) ;
 
-G = [0;
-    0;
-    0;
-    0;
-    0;
-    0;
-    -(1/m)*(sin(y(6))*sin(y(4))+cos(y(6))*cos(y(4))*sin(y(5)));
-    -(1/m)*(cos(y(4))*sin(y(6))-cos(y(6))*sin(y(4))*sin(y(5)));
-    - (1/m)*cos(y(6))*cos(y(5));
-    (1/Ix);
-    (1/Iy);
-    (1/Iz)];
+G1(7)=  -(1/m)*(sin(y(6))*sin(y(4))+cos(y(6))*cos(y(4))*sin(y(5))) ; 
+G1(8)=  -(1/m)*(cos(y(4))*sin(y(6))-cos(y(6))*sin(y(4))*sin(y(5))); 
+G1(9) =  - (1/m)*cos(y(6))*cos(y(5)); 
+
+G2(10) = 1/Ix ; 
+G3(11) = 1/Iy ; 
+G4(12) = 1/Iz ; 
+
+G = [G1 G2 G3 G4] ; 
+
+% G = [0;
+%     0;
+%     0;
+%     0;
+%     0;
+%     0;
+%     -(1/m)*(sin(y(6))*sin(y(4))+cos(y(6))*cos(y(4))*sin(y(5)));
+%     -(1/m)*(cos(y(4))*sin(y(6))-cos(y(6))*sin(y(4))*sin(y(5)));
+%     - (1/m)*cos(y(6))*cos(y(5));
+%     (1/Ix);
+%     (1/Iy);
+%     (1/Iz)];
 
 F = [y(7);
     y(8);
@@ -88,10 +103,9 @@ dF = [0 0 0 0 0   0    1 0 0 0     0     0;
       0 0 0 0 0   0    0 0 0 K1110 K1111 K1112        
       0 0 0 0 0   0    0 0 0 K1210 K1211 K1212 ] ;
       
-
+ 
 dydt2 =zeros(12,1);
-dydt2 = - (-Q*y(1:12) - inv(dF)*y(13:24));
-
+dydt2 = - (-Q*y(1:12) - dF'*y(13:24));
 
 %création de la matrice y finale (x+p)
 dydt = cat(1, dydt1, dydt2);
